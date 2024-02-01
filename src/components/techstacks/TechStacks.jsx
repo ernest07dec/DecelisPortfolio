d57f86ca19e026fe9f5bb1272f19cc704471524b
Ernest Fortune Decelis <ernestfortunedecelis@yahoo.com.ph>
2024-02-01 20:27:40 +0800

    refix techstacks

--- src/components/techstacks/TechStacks.jsx
+++ src/components/techstacks/TechStacks.jsx
@@ -5,64 +5,63 @@ import "swiper/css";
 // import required modules
 import { Autoplay } from "swiper/modules";
 import { TechStacksList } from "../jsonfiles/TechStacksList";
+import { useState } from "react";
 
 export const TechStacks = () => {
   // LANGUAGES
   const languages = TechStacksList;
 
+  const [loopIndex, setLoopIndex] = useState(0);
+
   return (
-    <>
-      <section className="w-screen bg-gray-200 px-10">
-        {/* SWIPER INITIALIZATION */}
-        <Swiper
-          breakpoints={{
-            0: {
-              slidesPerView: 4,
-              slidesPerGroup: 2,
-              initialSlide: 1,
-            },
-            480: {
-              slidesPerView: 5,
-              slidesPerGroup: 2,
-              initialSlide: 1,
-            },
-            768: {
-              slidesPerView: 6,
-              slidesPerGroup: 2,
-              initialSlide: 1,
-            },
-            1040: {
-              slidesPerView: 8,
-              slidesPerGroup: 3,
-              initialSlide: 1,
-            },
-          }}
-          spaceBetween={0}
-          centeredSlides={true}
-          autoplay={{
-            delay: 1000,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Autoplay]}
-        >
-          {/* SWIPER SLIDES */}
-          {languages.map((language, index) => {
-            return (
-              <SwiperSlide key={index}>
-                <div className="flex justify-center items-center rounded-3xl mx-auto p-5 lg:h-44 md:h-36  h-32 lg:w-44 md:w-36 w-32">
-                  <img
-                    src={language?.img}
-                    className="h-full w-full object-contain"
-                  />
-                </div>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-      </section>
-    </>
+    <section className="w-screen bg-gray-200 px-10">
+      <Swiper
+        loop
+        initialSlide={loopIndex}
+        breakpoints={{
+          0: {
+            slidesPerView: 4,
+            slidesPerGroup: 2,
+          },
+          480: {
+            slidesPerView: 5,
+            slidesPerGroup: 2,
+          },
+          768: {
+            slidesPerView: 6,
+            slidesPerGroup: 2,
+          },
+          1040: {
+            slidesPerView: 8,
+            slidesPerGroup: 3,
+          },
+        }}
+        spaceBetween={0}
+        centeredSlides={true}
+        autoplay={{
+          delay: 1000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        onSlideChange={(swiper) => {
+          setLoopIndex(swiper.realIndex % languages.length);
+        }}
+        modules={[Autoplay]}
+      >
+        {languages.map((language, index) => (
+          <SwiperSlide key={index}>
+            <div className="flex justify-center items-center rounded-3xl mx-auto p-5 lg:h-44 md:h-36 h-32 lg:w-44 md:w-36 w-32">
+              <img
+                src={language?.img}
+                alt={language?.name}
+                className="h-full w-full object-contain"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
   );
 };
--- src/pages/partial/PlayGround.jsx
+++ src/pages/partial/PlayGround.jsx
@@ -1,14 +1,77 @@
 // FRAMERMOTION
+import { useRef } from "react";
 import { AnimatedPage } from "../../components/framermotion/AnimatedPage";
 import { FrontendDemo } from "../../features/frontend_designs/FrontendDemo";
 import { Profile } from "../../features/profile/Profile";
+
+import { FaChevronCircleRight } from "react-icons/fa";
 export const PlayGround = () => {
+  const frontendDesignsRef = useRef(null);
+  const executeScroll = (ref) => {
+    if (ref && ref.current) {
+      window.scrollTo({
+        top: ref.current.offsetTop - 100,
+        left: 0,
+        behavior: "smooth",
+      });
+    }
+  };
   return (
     <AnimatedPage>
       {/* PROFILE */}
-      <Profile />
+      <section>
+        <Profile />
+      </section>
+      {/* WELCOME */}
+      <section className="max-w-screen-xl mx-auto mt-10 lg:px-0 px-5 sm:px-10 text-gray-100 flex flex-col gap-5 text-center">
+        <h3 className="text-4xl">Welcome to my Play Ground!</h3>
+        <div>
+          <p className="text-lg">
+            Greetings! I'm Ernest, a passionate developer exploring the vast
+            realms of web technologies and design. This digital playground
+            serves as a showcase of my skills and creativity, featuring a
+            variety of projects and demos.
+          </p>
+          <p>What You'll Find Here: Frontend Designs:</p>
+          <ul className="px-32 text-left flex flex-col gap-3 justify-center py-5 font-semibold">
+            <li className="flex items-center gap-2">
+              <FaChevronCircleRight />
+              Dive into visually appealing and responsive frontend designs that
+              showcase my design sensibilities.{" "}
+            </li>
+            <li className="flex items-center gap-2">
+              <FaChevronCircleRight />
+              Interactive Demos: Experience the magic of real-time communication
+              with demos highlighting technologies like Socket.IO and WebRTC.{" "}
+            </li>
+            <li className="flex items-center gap-2">
+              <FaChevronCircleRight className="text-xl" />
+              Get to know more about me, my skills, and the enthusiasm I bring
+              to every project. Feel free to explore, click around, and witness
+              the fusion of code and creativity with both Frontend and Backend
+              technologies.
+            </li>
+          </ul>
+          <p>
+            Ready to embark on a journey through the digital landscape? Let's
+            start the adventure!
+          </p>
+        </div>
+        <div className="h-20">
+          <div className="w-full h-full flex justify-center items-center">
+            <button
+              className="py-2 px-5 border-2 border-indigo-800 duration-500 hover:border-4 text-xl hover:font-semibold hover:animate-ping"
+              onClick={() => executeScroll(frontendDesignsRef)}
+            >
+              Let's Start!
+            </button>
+          </div>
+        </div>
+      </section>
       {/* FRONTEND DEMO */}
-      <FrontendDemo />
+      <section className="mt-32" ref={frontendDesignsRef}>
+        <FrontendDemo />
+      </section>
     </AnimatedPage>
   );
 };
