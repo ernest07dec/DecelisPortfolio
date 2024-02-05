b002e40bb94fd4ea471017e67e915baf0a8c278b
Ernest Fortune Decelis <ernestfortunedecelis@yahoo.com.ph>
2024-02-05 12:09:01 +0800

    reuploadcv

--- src/components/header_footer/partial/Header.js
+++ src/components/header_footer/partial/Header.js
@@ -46,7 +46,7 @@ export const Header = () => {
         </Link>
         <div className="flex md:order-2">
           <a
-            href="https://drive.google.com/file/d/1qUbNvuWJntLnxFdZ6pSt3OLXzaufsk1Q/view?usp=sharing"
+            href="https://drive.google.com/file/d/1YZcf7wz--En3vBmX_P2D_AtVNFuBlJ2a/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
